751f0938ef40512b836a7d1c3f63de6b122e8d71
kingflurkel <mikethuy@gmail.com>
2016-04-29 12:12:46 +0200

    ...

--- contracts/vote.sol
+++ contracts/vote.sol
@@ -1,8 +1,3 @@
-/* The token is used as a voting shares */
-contract token { mapping (address => uint256) public balanceOf;  }
-
-
-/* define 'owned' */
 contract owned {
     address public owner;
 
@@ -21,20 +16,22 @@ contract owned {
 }
 
 
-/* The democracy contract itself */
-contract Association is owned {
+contract Congress is owned {
 
     /* Contract Variables and events */
     uint public minimumQuorum;
     uint public debatingPeriodInMinutes;
+    int public majorityMargin;
     Proposal[] public proposals;
     uint public numProposals;
-    token public sharesTokenAddress;
+    mapping (address => uint) public memberId;
+    Member[] public members;
 
     event ProposalAdded(uint proposalID, address recipient, uint amount, string description);
-    event Voted(uint proposalID, bool position, address voter);
+    event Voted(uint proposalID, bool position, address voter, string justification);
     event ProposalTallied(uint proposalID, int result, uint quorum, bool active);
-    event ChangeOfRules(uint minimumQuorum, uint debatingPeriodInMinutes, address sharesTokenAddress);
+    event MembershipChanged(address member, bool isMember);
+    event ChangeOfRules(uint minimumQuorum, uint debatingPeriodInMinutes, int majorityMargin);
 
     struct Proposal {
         address recipient;
@@ -44,34 +41,74 @@ contract Association is owned {
         bool executed;
         bool proposalPassed;
         uint numberOfVotes;
+        int currentResult;
         bytes32 proposalHash;
         Vote[] votes;
         mapping (address => bool) voted;
     }
 
+    struct Member {
+        address member;
+        bool canVote;
+        string name;
+        uint memberSince;
+    }
+
     struct Vote {
         bool inSupport;
         address voter;
+        string justification;
     }
 
     /* modifier that allows only shareholders to vote and create new proposals */
-    modifier onlyShareholders {
-        if (sharesTokenAddress.balanceOf(msg.sender) == 0) throw;
+    modifier onlyMembers {
+        if (memberId[msg.sender] == 0
+        || !members[memberId[msg.sender]].canVote)
+        throw;
         _
     }
 
     /* First time setup */
-    function Association(token sharesAddress, uint minimumSharesToPassAVote, uint minutesForDebate) {
-        changeVotingRules(sharesAddress, minimumSharesToPassAVote, minutesForDebate);
+    function Congress(
+        uint minimumQuorumForProposals,
+        uint minutesForDebate,
+        int marginOfVotesForMajority, address congressLeader
+    ) {
+        changeVotingRules(minimumQuorumForProposals, minutesForDebate, marginOfVotesForMajority);
+        members.length++;
+        members[0] = Member({member: 0, canVote: false, memberSince: now, name: ''});
+        if (congressLeader != 0) owner = congressLeader;
+
+    }
+
+    /*make member*/
+    function changeMembership(address targetMember, bool canVote, string memberName) onlyOwner {
+        uint id;
+        if (memberId[targetMember] == 0) {
+           memberId[targetMember] = members.length;
+           id = members.length++;
+           members[id] = Member({member: targetMember, canVote: canVote, memberSince: now, name: memberName});
+        } else {
+            id = memberId[targetMember];
+            Member m = members[id];
+            m.canVote = canVote;
+        }
+
+        MembershipChanged(targetMember, canVote);
+
     }
 
     /*change rules*/
-    function changeVotingRules(token sharesAddress, uint minimumSharesToPassAVote, uint minutesForDebate) onlyOwner {
-        sharesTokenAddress = token(sharesAddress);
-        if (minimumSharesToPassAVote == 0 ) minimumSharesToPassAVote = 1;
-        minimumQuorum = minimumSharesToPassAVote;
+    function changeVotingRules(
+        uint minimumQuorumForProposals,
+        uint minutesForDebate,
+        int marginOfVotesForMajority
+    ) onlyOwner {
+        minimumQuorum = minimumQuorumForProposals;
         debatingPeriodInMinutes = minutesForDebate;
-        ChangeOfRules(minimumQuorum, debatingPeriodInMinutes, sharesTokenAddress);
+        majorityMargin = marginOfVotesForMajority;
+
+        ChangeOfRules(minimumQuorum, debatingPeriodInMinutes, majorityMargin);
     }
 
     /* Function to create a new proposal */
@@ -81,7 +118,7 @@ contract Association is owned {
         string JobDescription,
         bytes transactionBytecode
     )
-        onlyShareholders
+        onlyMembers
         returns (uint proposalID)
     {
         proposalID = proposals.length++;
@@ -112,51 +149,39 @@ contract Association is owned {
         return p.proposalHash == sha3(beneficiary, etherAmount, transactionBytecode);
     }
 
-    /* */
-    function vote(uint proposalNumber, bool supportsProposal)
-        onlyShareholders
+    function vote(
+        uint proposalNumber,
+        bool supportsProposal,
+        string justificationText
+    )
+        onlyMembers
         returns (uint voteID)
     {
-        Proposal p = proposals[proposalNumber];
-        if (p.voted[msg.sender] == true) throw;
-
-        voteID = p.votes.length++;
-        p.votes[voteID] = Vote({inSupport: supportsProposal, voter: msg.sender});
-        p.voted[msg.sender] = true;
-        p.numberOfVotes = voteID +1;
-        Voted(proposalNumber,  supportsProposal, msg.sender);
+        Proposal p = proposals[proposalNumber];         // Get the proposal
+        if (p.voted[msg.sender] == true) throw;         // If has already voted, cancel
+        p.voted[msg.sender] = true;                     // Set this voter as having voted
+        p.numberOfVotes++;                              // Increase the number of votes
+        if (supportsProposal) {                         // If they support the proposal
+            p.currentResult++;                          // Increase score
+        } else {                                        // If they don't
+            p.currentResult--;                          // Decrease the score
+        }
+        // Create a log of this event
+        Voted(proposalNumber,  supportsProposal, msg.sender, justificationText);
     }
 
     function executeProposal(uint proposalNumber, bytes transactionBytecode) returns (int result) {
         Proposal p = proposals[proposalNumber];
         /* Check if the proposal can be executed */
-        if (now < p.votingDeadline  /* has the voting deadline arrived? */
-            ||  p.executed        /* has it been already executed? */
-            ||  p.proposalHash != sha3(p.recipient, p.amount, transactionBytecode)) /* Does the transaction code match the proposal? */
+        if (now < p.votingDeadline                                                  // has the voting deadline arrived?
+            || p.executed                                                           // has it been already executed?
+            || p.proposalHash != sha3(p.recipient, p.amount, transactionBytecode)   // Does the transaction code match the proposal?
+            || p.numberOfVotes < minimumQuorum)                                    // has minimum quorum?
             throw;
-
-        /* tally the votes */
-        uint quorum = 0;
-        uint yea = 0;
-        uint nay = 0;
-
-        for (uint i = 0; i <  p.votes.length; ++i) {
-            Vote v = p.votes[i];
-            uint voteWeight = sharesTokenAddress.balanceOf(v.voter);
-            quorum += voteWeight;
-            if (v.inSupport) {
-                yea += voteWeight;
-            } else {
-                nay += voteWeight;
-            }
-        }
 
         /* execute result */
-        if (quorum <= minimumQuorum) {
-            /* Not enough significant voters */
-            throw;
-        } else if (yea > nay ) {
-            /* has quorum and was approved */
+        if (p.currentResult > majorityMargin) {
+            /* If difference between support and opposition is larger than margin */
             p.recipient.call.value(p.amount * 1 ether)(transactionBytecode);
             p.executed = true;
             p.proposalPassed = true;
@@ -165,6 +190,6 @@ contract Association is owned {
             p.proposalPassed = false;
         }
         // Fire Events
-        ProposalTallied(proposalNumber, result, quorum, p.proposalPassed);
+        ProposalTallied(proposalNumber, p.currentResult, p.numberOfVotes, p.proposalPassed);
     }
 }
